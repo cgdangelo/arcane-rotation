41dcac9022fc175a3447a9b7576043d54f15dd45
Charles D'Angelo <c.g.dangelo@gmail.com>
2019-11-18 18:00:55 -0500

    Add embarrassing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,160 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { all, fork } from "redux-saga/effects";
+import "./App.css";
+import {
+  castAttempt,
+  castWatcher,
+  getCasting,
+  globalCooldown,
+  manaRegen,
+  Resource,
+  rootReducer
+} from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(function* rootSaga() {
+  yield all([fork(manaRegen), fork(castWatcher), fork(globalCooldown)]);
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Provider store={store}>
+      <div className="App">
+        <AbilityTimeline />
+
+        <ArcaneCharges />
+        <ManaBar />
+        <CastBar />
+        <ActionBar />
+      </div>
+    </Provider>
+  );
+};
+
+const ArcaneCharges: React.FC = () => {
+  const arcaneCharges = useSelector(
+    (state: ReturnType<typeof rootReducer>) =>
+      state.resources[Resource.ARCANE_CHARGES]
+  );
+
+  const arcaneChargeBars = [];
+
+  for (
+    let arcaneChargeIndex = 1;
+    arcaneChargeIndex <= arcaneCharges.max;
+    arcaneChargeIndex++
+  ) {
+    arcaneChargeBars.push(
+      <div
+        key={arcaneChargeIndex}
+        className={`ArcaneCharges__charge${
+          arcaneChargeIndex <= arcaneCharges.current
+            ? " ArcaneCharges__charge--charged"
+            : ""
+        }`}
+      />
+    );
+  }
+
+  return <div className="ArcaneCharges">{arcaneChargeBars}</div>;
+};
+
+const ManaBar: React.FC = () => {
+  const mana = useSelector(
+    (state: ReturnType<typeof rootReducer>) => state.resources[Resource.MANA]
+  );
+
+  return (
+    <div className="ManaBar">
+      <span
+        className="ManaBar__bar"
+        style={{ width: `${(mana.current / mana.max) * 100}%` }}
+      />
     </div>
   );
-}
+};
+
+const ActionBar: React.FC = () => {
+  return (
+    <div className="ActionBar">
+      <ActionBarAbility abilityName="Arcane Blast" />
+      <ActionBarAbility abilityName="Arcane Barrage" />
+    </div>
+  );
+};
+
+const ActionBarAbility: React.FC<{ abilityName: string }> = ({
+  abilityName
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <i
+      className="ActionBarAbility"
+      onClick={() => {
+        dispatch(castAttempt(abilityName));
+      }}
+    >
+      {abilityName}
+    </i>
+  );
+};
+
+const AbilityTimeline: React.FC = () => {
+  const abilityTimeline = useSelector(
+    (state: ReturnType<typeof rootReducer>) => state.abilityTimeline
+  );
+
+  return (
+    <dl className="AbilityTimeline">
+      {abilityTimeline.map(([timestamp, abilityName]) => (
+        <React.Fragment key={timestamp}>
+          <dt>{timestamp}</dt>
+          <dd>{abilityName}</dd>
+        </React.Fragment>
+      ))}
+    </dl>
+  );
+};
+
+const CastBar: React.FC = () => {
+  const casting = useSelector(getCasting);
+  const [castFinishTime, setCastFinishTime] = useState(0);
+  const [castTimer, setCastTimer] = useState(0);
+
+  return (
+    <div>
+      <h2>
+        {casting ? (
+          <>
+            Casting: {casting.abilityName} {castTimer} / {casting.castTime}
+            <span
+              style={{
+                background: "#fff",
+                display: "block",
+                height: "1rem",
+                width: `${(castTimer / casting.castTime) * 100}%`
+              }}
+            />
+          </>
+        ) : (
+          <>Idle</>
+        )}
+      </h2>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/sagas.ts
@@ -0,0 +1,322 @@
+import { Reducer } from "redux";
+import { delay, fork, put, select, take } from "redux-saga/effects";
+
+const MANA_MAX = 100000;
+
+export enum Attribute {
+  CRITICAL_HIT = "crit",
+  HASTE = "haste",
+  INTELLECT = "int",
+  MASTERY = "mast",
+  VERSATILITY = "vers"
+}
+
+export enum Resource {
+  ARCANE_CHARGES = "arcane_charge",
+  MANA = "mana"
+}
+
+interface State {
+  abilityTimeline: [number, string][];
+  attributes: Record<Attribute, number>;
+  buffs: {
+    [buffName: string]: {
+      applied: number;
+      expires: number;
+      stacks: number;
+    };
+  };
+  casting: {
+    abilityName: string;
+    castTime: number;
+  } | null;
+  gcdLocked: boolean;
+  resources: {
+    [R in Resource]: {
+      current: number;
+      max: number;
+      min: number;
+    };
+  };
+}
+
+const getCrit = (state: State) => state.attributes[Attribute.CRITICAL_HIT];
+const getHaste = (state: State) => state.attributes[Attribute.HASTE];
+const getInt = (state: State) => state.attributes[Attribute.INTELLECT];
+const getMastery = (state: State) => state.attributes[Attribute.MASTERY];
+const getVers = (state: State) => state.attributes[Attribute.VERSATILITY];
+const getArcaneCharges = (state: State) =>
+  state.resources[Resource.ARCANE_CHARGES];
+const getMana = (state: State) => state.resources[Resource.MANA];
+const hasArcanePower = (state: State) => state.buffs["Arcane Power"];
+export const getCasting = (state: State) => state.casting;
+
+export const castAttempt = (abilityName: string) => ({
+  type: "CAST_ATTEMPT" as const,
+  payload: {
+    timestamp: Date.now(),
+    abilityName
+  }
+});
+
+const triggerGcd = (gcdLength = 1500) => ({
+  type: "TRIGGER_GCD" as const,
+  payload: gcdLength
+});
+
+const updateGcdLock = (lock = true) => ({
+  type: "GCD" as const,
+  payload: lock
+});
+
+const spellCastStart = (abilityName: string, castTime: number = 0) => ({
+  type: "SPELL_CAST_START" as const,
+  payload: {
+    timestamp: Date.now(),
+    abilityName,
+    castTime
+  }
+});
+
+const spellCastFailed = (abilityName: string) => ({
+  type: "SPELL_CAST_FAILED" as const,
+  payload: {
+    timestamp: Date.now(),
+    abilityName
+  }
+});
+
+const spellCastSuccess = (abilityName: string) => ({
+  type: "SPELL_CAST_SUCCESS" as const,
+  payload: {
+    timestamp: Date.now(),
+    abilityName
+  }
+});
+
+const setResourceValue = (resource: Resource, value: number) => ({
+  type: "SET_RESOURCE_VALUE" as const,
+  payload: {
+    timestamp: Date.now(),
+    resource,
+    value
+  }
+});
+
+const triggerDamage = (abilityName: string, value: number) => ({
+  type: "SPELL_DAMAGE" as const,
+  payload: {
+    timestamp: Date.now(),
+    abilityName,
+    value
+  }
+});
+
+const defaultState: State = {
+  abilityTimeline: [],
+  attributes: {
+    [Attribute.CRITICAL_HIT]: 0.2,
+    [Attribute.HASTE]: 0.2,
+    [Attribute.INTELLECT]: 10000,
+    [Attribute.MASTERY]: 0.2,
+    [Attribute.VERSATILITY]: 0.2
+  },
+  buffs: {},
+  casting: null,
+  gcdLocked: false,
+  resources: {
+    [Resource.ARCANE_CHARGES]: {
+      current: 0,
+      max: 4,
+      min: 0
+    },
+    [Resource.MANA]: {
+      current: 100000,
+      max: 100000,
+      min: 0
+    }
+  }
+};
+
+type Action = ReturnType<
+  | typeof updateGcdLock
+  | typeof spellCastStart
+  | typeof spellCastFailed
+  | typeof spellCastSuccess
+  | typeof setResourceValue
+  | typeof triggerDamage
+>;
+
+export const rootReducer: Reducer<State, Action> = (
+  state = defaultState,
+  action
+) => {
+  switch (action.type) {
+    case "GCD":
+      return { ...state, gcdLocked: action.payload };
+
+    case "SPELL_CAST_START":
+      return { ...state, casting: action.payload };
+
+    case "SPELL_CAST_SUCCESS":
+      return {
+        ...state,
+        abilityTimeline: [
+          [action.payload.timestamp, action.payload.abilityName],
+          ...state.abilityTimeline
+        ],
+        casting: null
+      };
+
+    case "SET_RESOURCE_VALUE":
+      if (!(action.payload.resource in state.resources)) {
+        return state;
+      }
+
+      return {
+        ...state,
+        resources: {
+          ...state.resources,
+          [action.payload.resource]: {
+            ...state.resources[action.payload.resource],
+            current: action.payload.value
+          }
+        }
+      };
+
+    default:
+      return state;
+  }
+};
+
+export function* manaRegen() {
+  while (true) {
+    yield delay(1000);
+
+    const mana = yield select(getMana);
+    const mastery = yield select(getMastery);
+
+    if (mana.current < mana.max) {
+      yield put(
+        setResourceValue(
+          Resource.MANA,
+          Math.min(mana.current + 0.02 * mana.max * (1 + mastery), mana.max)
+        )
+      );
+    }
+  }
+}
+
+export function* globalCooldown() {
+  while (true) {
+    const { payload } = yield take("TRIGGER_GCD");
+
+    // Set the GCD lock.
+    yield put(updateGcdLock());
+
+    // Get character haste.
+    const haste = yield select(getHaste);
+
+    // Calculate hasted GCD length (0.75s minimum).
+    const gcdLength = Math.min(0.75, payload / (1 + haste));
+
+    // Wait for GCD to expire.
+    yield delay(gcdLength);
+
+    // Unlock the GCD.
+    yield put(updateGcdLock(false));
+  }
+}
+
+export function* castWatcher() {
+  while (true) {
+    const {
+      payload: { abilityName }
+    } = yield take("CAST_ATTEMPT");
+
+    switch (abilityName) {
+      case "Arcane Blast":
+        yield fork(arcaneBlast);
+        break;
+
+      case "Arcane Barrage":
+        yield fork(arcaneBarrage);
+        break;
+    }
+  }
+}
+
+export function* arcaneBlast() {
+  const casting = yield select(getCasting);
+
+  if (casting !== null) {
+    return yield put(spellCastFailed("Arcane Blast"));
+  }
+
+  yield put(triggerGcd());
+
+  const { current: currentArcaneCharges, max: maxArcaneCharges } = yield select(
+    getArcaneCharges
+  );
+  const haste = yield select(getHaste);
+
+  const castTime =
+    2250 * (1 / (1 + haste)) * (1.0 - 0.08 * currentArcaneCharges);
+
+  yield put(spellCastStart("Arcane Blast", castTime));
+
+  yield delay(castTime);
+
+  const arcanePower = yield select(hasArcanePower);
+
+  const { current: currentMana } = yield select(getMana);
+  const cost =
+    0.0275 * MANA_MAX * (1 + currentArcaneCharges) * (arcanePower ? 0.8 : 1);
+
+  if (currentMana < cost) {
+    yield put(spellCastFailed("Arcane Blast"));
+
+    return false;
+  }
+
+  yield put(spellCastSuccess("Arcane Blast"));
+
+  yield put(setResourceValue(Resource.MANA, Math.max(currentMana - cost, 0)));
+
+  const int = yield select(getInt);
+  const crit = yield select(getCrit);
+  const mast = yield select(getMastery);
+  const vers = yield select(getVers);
+
+  yield put(
+    triggerDamage(
+      "Arcane Blast",
+      0.55 *
+        int *
+        (Math.random() < crit ? 2 : 1) *
+        (1 + vers) *
+        (1 + (0.6 + mast / 2) * currentArcaneCharges) *
+        (arcanePower ? 1.3 : 1)
+    )
+  );
+
+  if (currentArcaneCharges < maxArcaneCharges) {
+    yield put(
+      setResourceValue(Resource.ARCANE_CHARGES, currentArcaneCharges + 1)
+    );
+  }
+
+  return true;
+}
+
+export function* arcaneBarrage() {
+  const casting = yield select(getCasting);
+
+  if (casting !== null) {
+    return yield put(spellCastFailed("Arcane Barrage"));
+  }
+
+  yield put(triggerGcd());
+
+  yield put(setResourceValue(Resource.ARCANE_CHARGES, 0));
+}
